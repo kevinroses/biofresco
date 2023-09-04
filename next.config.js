cabca845e44f8414308649bdcc8b1e2f94137e3f
kentur <kevinxrturop@gmail.com>
2023-09-04 11:28:08 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  experimental: {
+    pageDataCollectionTimeout: 10000, // Aumenta el tiempo de espera según sea necesario
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
