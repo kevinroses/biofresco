847c94a88e124587b351ab9b8cc0c4eeef721f48
kentur <kevinxrturop@gmail.com>
2023-09-04 16:15:36 -0500

    Update formatedDays.js

--- src/utils/formatedDays.js
+++ src/utils/formatedDays.js
@@ -1,7 +1,7 @@
 import moment from "moment";
 
-export const currentDate = moment().format("YYYY/MM/DD HH:mm");
-export const nextday = moment(currentDate).add(1, "days").format("YYYY/MM/DD");
+export const currentDate = moment().format("MM-DD-YYYY HH:mm");
+export const nextday = moment(currentDate).add(1, "days").format("MM-DD-YYYY");
 
 export const today = moment(currentDate).format("dddd");
 export const tomorrow = moment(nextday).format("dddd");
